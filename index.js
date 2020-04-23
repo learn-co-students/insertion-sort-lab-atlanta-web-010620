5a994b9d8ba66d1a01cac30026ec5e0774b2c28c
Mitchell Goodwin <mgoodwin777@gmail.com>
2020-04-23 13:13:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 function findMinAndRemove(array){
-
+    let minNum = array[0]
+    let minIndex = 0
+    for(let i = 0; i < array.length; i++) {
+        let currentNum = array[i]
+        if (currentNum < minNum) {
+            minNum = currentNum
+            minIndex = i
+        }
+    }
+    array.splice(minIndex, 1)
+    return minNum
 }
 
 function insertionSort(array){
-
+    let newMin
+    let sorted = []
+    while (array.length > 0) {
+        newMin = findMinAndRemove(array)
+        sorted.push(newMin)
+    }
+    return sorted
 }
