7e1bd76436996821b8ed824647d723e30c467b43
tylergreason <tyler.greason@protonmail.com>
2020-05-18 12:36:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 function findMinAndRemove(array){
-
+    array.sort((a,b) => a-b) 
+    let min = array[0]
+    array.shift() 
+    return min; 
 }
 
 function insertionSort(array){
-
+    let newArray = []; 
+    while (array.length > 0){
+        newArray.push(findMinAndRemove(array)); 
+    }
+    return newArray;
 }
